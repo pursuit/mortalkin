1684997886e1e92d35eeec6d0a0e2df1ebce64e3
danielbintar <daniel.bintar@bukalapak.com>
2021-06-06 08:21:12 +0700

    fix port (#2)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,7 +17,7 @@ import (
 func main() {
 	defer log.Println("Shutdown the server success")
 
-	portalConn, err := grpc.Dial(":5004", grpc.WithInsecure())
+	portalConn, err := grpc.Dial(":5001", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
