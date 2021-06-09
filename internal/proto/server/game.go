82363b0f64d0900b5a133c453c433bb71a74a889
@danielbintar <daniel.bintar@bukalapak.com>
2021-06-09 22:12:02 +0700

    api

---
+++ internal/proto/server/game.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"io"
+
+	"github.com/pursuit/mortalkin/internal/game"
+	"github.com/pursuit/mortalkin/internal/proto/out/api/mortalkin"
+	"github.com/pursuit/mortalkin/internal/proto/out/api/portal"
+
+	"github.com/pursuit/portal/pkg"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type GameServer struct {
+	mortalkin_proto.UnimplementedUserServer
+
+	UserClient portal_proto.UserClient
+}
+
+func (this GameServer) Play(stream mortalkin_proto.Game_PlayServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		var claims pkg.Jwt
+		_, err = jwt.ParseWithClaims(in.GetToken(), &claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte("zxcwqe"), nil
+		})
+		if err != nil {
+			return err
+		}
+
+		return game.Connect(int(in.GetCharacterId()), claims.ID, stream)
+	}
+}
