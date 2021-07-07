ec9f843741676dfbef59affaf3d671e8b3381c20
@danielbintar <daniel.bintar@bukalapak.com>
2021-07-07 21:14:20 +0700

    snapshot function

--- internal/game/base.go
+++ internal/game/base.go
@@ -99,32 +99,7 @@ func Prepare() {
 }
 
 func StartServer() {
-	go func() {
-		cnt := 0
-		ticker := time.NewTicker(1 * time.Minute)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				writeSnapshot()
-				cnt += 1
-				if cnt >= 5 {
-					cnt = 0
-					files, err := ioutil.ReadDir("resource/snapshot")
-					if err != nil {
-						panic(err)
-					}
-
-					for i := 1; i+1 < len(files); i += 1 {
-						filename := fmt.Sprintf("resource/snapshot/%s", files[i].Name())
-						if err := os.Remove(filename); err != nil {
-							panic(err)
-						}
-					}
-				}
-			}
-		}
-	}()
+	go periodicallyWriteSnapshot()
 
 	ticker := time.NewTicker(16 * time.Millisecond)
 	defer ticker.Stop()
---
+++ internal/game/snapshot.go
@@ -0,0 +1,35 @@
+package game
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+func periodicallyWriteSnapshot() {
+	cnt := 0
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			writeSnapshot()
+			cnt += 1
+			if cnt >= 5 {
+				cnt = 0
+				files, err := ioutil.ReadDir("resource/snapshot")
+				if err != nil {
+					panic(err)
+				}
+
+				for i := 1; i+1 < len(files); i += 1 {
+					filename := fmt.Sprintf("resource/snapshot/%s", files[i].Name())
+					if err := os.Remove(filename); err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+	}
+}
